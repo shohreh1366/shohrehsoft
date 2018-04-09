3c7947ad56ecc462f90b2ff49b01b0346ebd511c
Shohreh Saber <shohrehsaber@yahoo.com>
2018-04-08 20:39:53 -0700

    added more qualities

--- src/shohrehsoft/HelloWorld.java
+++ src/shohrehsoft/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Navid is God");
-		System.out.println("And... Shohreh is a Goddess and professional Java Developer.");
+		System.out.println("And... Shohreh is a Goddess and professional Java Developer and also rocks in C#.");
 	}
 
 }
