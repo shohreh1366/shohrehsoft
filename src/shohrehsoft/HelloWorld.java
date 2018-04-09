1b9b9e3783a4c7a788e06f0e9e43bcd08c6d02ef
Shohreh Saber <shohrehsaber@yahoo.com>
2018-04-08 20:33:00 -0700

    Initial checkin

---
+++ src/shohrehsoft/HelloWorld.java
@@ -0,0 +1,12 @@
+package shohrehsoft;
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Navid is God");
+		System.out.println("And... Shohreh is a Goddess");
+	}
+
+}
